4530081be403e22f357f39a1edd80db4febac825
Siraprapa Patta <siraprapa.pat@kkumail.com>
2025-03-27 02:31:22 +0700

    แก้ไข

--- server/server.js
+++ server/server.js
@@ -1357,6 +1357,6 @@ app.get('/blogs/by-location/:locationname', async (req, res) => {
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is Running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
